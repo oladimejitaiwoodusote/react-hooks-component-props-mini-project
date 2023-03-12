0c2e0b1b8238899ec92d32000b47476af43d7208
Oladimeji Odusote <taiwo.odusote@yahoo.com>
2023-03-12 16:31:30 -0400

    react

---
+++ src/components/Article.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Article({
+    date = "January 1, 1970",
+    title,
+    preview
+}){
+    return(
+        <article>
+            <h3>{title}</h3>
+            <small>{date}</small>
+            <p>{preview}</p>
+        </article>
+    )
+}
+
+export default Article
